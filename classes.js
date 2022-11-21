392fdf4b793aa5dc1ef589f7adc4a81c6154f7fe
treeden123 <treeden1@yahoo.co.nz>
2022-11-22 10:30:24 +1300

    done

--- classes.js
+++ classes.js
@@ -1,16 +1,18 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
+function start() {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one() {
   // First, we have to find the element:
   let one = document.getElementById('one')
 
@@ -19,7 +21,21 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two() {
+  let two = document.getElementById('two')
 
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
+function three() {
+  let three = document.getElementById('three')
 
+  three.classList.add('orange')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  let makeVisible = document.getElementsByClassName('invisible')[0]
+  console.log(makeVisible)
+
+  makeVisible.classList.add('visible')
+}
